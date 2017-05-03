caf79436199454fabd554e8a8afcb902ab89d35a
philipmclifton <me@philipmclifton.com>
2017-05-03 17:08:03 +0100

    Update RestClient.cs

--- src/DotNetClient/DotNetClient/Wonde/Helpers/RestClient.cs
+++ src/DotNetClient/DotNetClient/Wonde/Helpers/RestClient.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -144,12 +144,7 @@ namespace Wonde.Helpers
 
                 if (!string.IsNullOrEmpty(PostData) && Method == HttpVerb.POST)
                 {
-                    var encoding = new UTF8Encoding();
-<<<<<<< HEAD
                     var bytes = Encoding.UTF8.GetBytes(PostData);
-=======
-                    var bytes = Encoding.GetEncoding("utf8").GetBytes(PostData);
->>>>>>> origin/development
                     request.ContentLength = bytes.Length;
 
                     using (var writeStream = request.GetRequestStream())
