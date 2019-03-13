e69c30fa0691015f371916714aead82c29f80aa3
Darren Hodges <dhodges@wonde.com>
2019-03-13 15:06:15 +0000

    Version 1.0.6

--- src/DotNetClient/DotNetClient/Wonde/Client.cs
+++ src/DotNetClient/DotNetClient/Wonde/Client.cs
@@ -16,7 +16,7 @@ namespace Wonde
         /// <summary>
         /// Setting version for the client
         /// </summary>
-        public const string VERSION = "1.0.5";
+        public const string VERSION = "1.0.6";
 
         /// <summary>
         /// Token for accessing the server
